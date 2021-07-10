1f2890c08d7ec9f0b7b0b553ed40e0c3241a8bee
andrebianchessi <andrebianchessi@gmail.com>
2021-07-09 23:57:01 -0300

    system integration separated

--- Assignment 3/ex3/main.py
+++ Assignment 3/ex3/main.py
@@ -13,7 +13,7 @@ def Sin(x):
 def main(case):
     # time parameters
     ti = 0.0
-    tf = 2
+    tf = 10
 
     # constants
     l = 10
@@ -38,12 +38,18 @@ def main(case):
         cTheta = math.cos(theta)
         return -2*thetaDot*phiDot*cTheta/sTheta
     
-    def XDotDot(x,y,z):
-        return x*(-1-g*z/(l*l))
-    def YDotDot(x,y,z):
-        return y*(-g*z/(l*l)-1)
-    def ZDotDot(x,y,z):
-        return -z+g*(x*x+y*y)/(l*l)
+    def B(xDot,yDot,zDot):
+        return -(xDot*xDot+yDot*yDot+zDot*zDot)
+
+    def XDotDot(x, xDot, y, yDot,z, zDot):
+        b = B(xDot,yDot,zDot)
+        return x*(b-g*z/(l*l))
+    def YDotDot(x, xDot, y, yDot,z, zDot):
+        b = B(xDot,yDot,zDot)
+        return y*(b-g*z/(l*l))
+    def ZDotDot(x, xDot, y, yDot,z, zDot):
+        b = B(xDot,yDot,zDot)
+        return (g*(x*x+y*y)+b*z)/(l*l)
 
     def Xf(theta, phi):
         return l*math.sin(theta)*math.sin(phi)
@@ -60,42 +66,46 @@ def main(case):
     yDot0 = l*(Cos(theta0)*Cos(phi0)*thetaDot0 - Sin(theta0)*Sin(phi0)*phiDot0)
     zDot0 = l*Sin(theta0)*thetaDot0
 
-    u0 = [theta0, thetaDot0, phi0, phiDot0, x0, xDot0, y0, yDot0, z0, zDot0]
-
-    def du(u, t, par):
+    uSpherical0 = [theta0, thetaDot0, phi0, phiDot0]
+    def duSpherical(u, t, par):
         theta = u[0]
         thetaDot = u[1]
         phi = u[2]
         phiDot = u[3]
-        x = u[4]
-        xDot = u[5]
-        y = u[6]
-        yDot = u[7]
-        z = u[8]
-        zDot = u[9]
         return [
             thetaDot,
             ThetaDotDot(theta, phiDot),
             phiDot,
-            PhiDotDot(theta, thetaDot, phiDot),
+            PhiDotDot(theta, thetaDot, phiDot)
+        ]
+    uCartesian0 = [x0, xDot0, y0, yDot0, z0, zDot0]
+    def duCartesian(u, t, par):
+        x = u[0]
+        xDot = u[1]
+        y = u[2]
+        yDot = u[3]
+        z = u[4]
+        zDot = u[5]
+        return [
             xDot,
-            XDotDot(x,y,z),
+            XDotDot(x, xDot, y, yDot,z, zDot),
             yDot,
-            YDotDot(x,y,z),
+            YDotDot(x, xDot, y, yDot,z, zDot),
             zDot,
-            ZDotDot(x,y,z)
+            ZDotDot(x, xDot, y, yDot,z, zDot)
         ]
 
     # time step
     h = 0.01
     t = np.arange(ti, tf, h)
 
-    u = ode(du, u0, t, args=([],))
+    uSpherical = ode(duSpherical, uSpherical0, t, args=([],))
+    uCartesian = ode(duCartesian, uCartesian0, t, args=([],))
 
     initConds = "theta0=" + str(round(theta0/math.pi,2)) + "*pi, thetaDot0="+str(thetaDot0) + ", phiDot0="+str(phiDot0)
     # graphs
     plt.figure()
-    plt.plot(u[:,0], u[:,1])
+    plt.plot(uSpherical[:,0], uSpherical[:,1])
     plt.title("Phase space\n"+initConds)
     plt.xlabel("theta")
     plt.ylabel("thetaDot")
@@ -104,9 +114,9 @@ def main(case):
 
     plt.figure()
     if (phiDot0 != 0):
-        plt.plot(u[:,2], u[:,3])
+        plt.plot(uSpherical[:,2], u[:,3])
     else:
-        plt.scatter(u[:,2], u[:,3])
+        plt.scatter(uSpherical[:,2], uSpherical[:,3])
     plt.title("Phase space\n"+initConds)
     plt.xlabel("phi")
     plt.ylabel("phiDot")
@@ -118,9 +128,9 @@ def main(case):
     Y = []
     Z = []
 
-    for i in range(len(u[:,0])):
-        theta = u[i,0]
-        phi = u[i,2]
+    for i in range(len(uSpherical[:,0])):
+        theta = uSpherical[i,0]
+        phi = uSpherical[i,2]
         X.append(Xf(theta,phi))
         Y.append(Yf(theta,phi))
         Z.append(Zf(theta,phi))
@@ -128,12 +138,11 @@ def main(case):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d', title = "3D Plot of Motion\n"+initConds)
     ax.plot(np.array(X),np.array(Y),np.array(Z))
-    ax.plot(u[:,4],u[:,6], u[:,8])
+    ax.plot(uCartesian[:,0],uCartesian[:,2], uCartesian[:,4])
     ax.set_yscale('linear')
     ax.set_xscale('linear')
     ax.set_zscale('linear')
     plt.savefig('plots/ex3Case'+str(case)+'_3d.png')
-    plt.show()
 
 
 
